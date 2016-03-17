0ebd3ad9b51ce5ab25f5fc7b57e5906e9df9c0dd
zzj <adobemelove@163.com>
2016-03-17 10:39:27 +0800

    ss

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+
+var json={
+	"type":"url",
+	"title":"华夏基金管家",
+	"message":"您最贴心的理财小管家",
+	"image":"http://www.umeng.com/images/pic/social/integrated_3.png",
+	"url":"http://baidu.com",
+	"music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3",
+	"video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html"    	
+};
+var amcfm_utils={
+	showShare:function(json){
+		alert('标题拿到的参数=>----json'+json);
+	},
+	share:function(json){
+		alert('页面内分享=>-----json'+json);
+	}
+	
+};
+
+
+
