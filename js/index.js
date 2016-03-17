93fe08a1f9d80936668e43c20725242b358f678f
zzjangular <adobemelove@163.com>
2016-03-17 14:42:18 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
 
 
-var json={
-	"type":"url",
-	"title":"华夏基金管家",
-	"message":"您最贴心的理财小管家",
-	"image":"http://www.umeng.com/images/pic/social/integrated_3.png",
-	"url":"http://baidu.com",
-	"music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3",
-	"video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html"    	
-};
+
+var json='{"type":"url","title":"华夏基金管家","message":"您最贴心的理财小管家","image":"http://www.umeng.com/images/pic/social/integrated_3.png","url":"http://baidu.com","music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3","video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html" }';
+
+
+
+
 
 
 
