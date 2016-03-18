e55ed10caeada9c5ece37590f569f4d69c6e510b
zzjangular <adobemelove@163.com>
2016-03-18 13:33:16 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@
 
 
 var json='{"type":"url","title":"华夏基金管家","message":"您最贴心的理财小管家","image":"http://www.umeng.com/images/pic/social/integrated_3.png","url":"http://baidu.com","music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3","video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html" }';
-
+var json0='{"type":"image","title":"华夏基金管家","message":"您最贴心的理财小管家","image":"http://www.umeng.com/images/pic/social/integrated_3.png","url":"http://baidu.com","music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3","video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html" }';
+var json1='{"type":"video","title":"华夏基金管家","message":"您最贴心的理财小管家","image":"http://www.umeng.com/images/pic/social/integrated_3.png","url":"http://baidu.com","music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3","video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html" }';
+var json2='{"type":"music","title":"华夏基金管家","message":"您最贴心的理财小管家","image":"http://www.umeng.com/images/pic/social/integrated_3.png","url":"http://baidu.com","music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3","video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html" }';
+var json3='{"type":"url","title":"华夏基金管家","message":"您最贴心的理财小管家","image":"http://www.umeng.com/images/pic/social/integrated_3.png","url":"http://baidu.com","music":"http://music.huoxing.com/upload/20130330/1364651263157_1085.mp3","video":"http://video.sina.com.cn/p/sports/cba/v/2013-10-22/144463050817.html" }';
 
 
 
