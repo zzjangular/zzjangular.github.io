7ca7d4b5665e116e40e2fadd52b63274e733ee57
dancing fade <adobemelove@163.com>
2017-05-23 11:12:57 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,19 +1,4 @@
+$(document).ready(function(){
+	alert("1111")
 
-function Web(){
-	this.x=0;
-	this.y=0;
-	this.scale=0.5;	
-}
-Web.prototype.draw=function(gd){
-	gd.save();
-	
-	gd.translate(this.x,this.y);
-	gd.scale(this.scale,this.scale);
-	gd.drawImage(
-		JSON.web,
-		14,414,110,108,
-		-55,-54,110,108
-	);
-	
-	gd.restore();
-};
+})
